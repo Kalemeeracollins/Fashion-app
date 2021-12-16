18ddd66927c0fa91b3ac397496e380e6854316d0
Kalemeera Collins <kalemeeracollins@outlook.com>
2021-12-17 02:15:21 +0500

    Fashion app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Content } from 'carbon-components-react';
+import './app.scss';
+//import 'bootstrap/dist/css/bootstrap.css';
+import TutorialHeader from './components/TutorialHeader';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <BrowserRouter>
+      <TutorialHeader />
+      <Routes>
+        <Route exact path="/" element={< LandingPage />} />
+        <Route path="/repos" element={< RepoPage />} />
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
 
 export default App;
+
+//https://react.carbondesignsystem.com/?selectedKind=DataTable&selectedStory=with%20expansion
+//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
